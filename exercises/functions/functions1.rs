ea0f0e7b7c539848a013ad0166dcc899d0369f24
yrd241 <yrd241@gmail.com>
2023-05-05 17:40:54 +0800

    updates

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,7 +1,11 @@
 // functions1.rs
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+
+fn call_me() -> String {
+    "hello world".to_string()
+}
 
 fn main() {
     call_me();
