23933438920789b976739b930ce4dd8e3cc710dd
yrd241 <yrd241@gmail.com>
2023-05-19 15:34:24 +0800

    updates

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -3,7 +3,6 @@
 // can offer. Follow the steps to complete the exercise.
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -12,7 +11,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_uppercase().to_string() + &input[1..],
     }
 }
 
@@ -21,7 +20,11 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut res:Vec<String> = vec![];
+    for s in words.iter() {
+        res.push(capitalize_first(s));
+    }
+    res
 }
 
 // Step 3.
@@ -29,7 +32,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut res:String = String::new();
+    for s in words.iter() {
+        res += &capitalize_first(s);
+    }
+    res
 }
 
 #[cfg(test)]
--- exercises/tests/tests4.rs
+++ exercises/tests/tests4.rs
@@ -2,7 +2,6 @@
 // Make sure that we're testing for the correct conditions!
 // Execute `rustlings hint tests4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 struct Rectangle {
     width: i32,
@@ -11,7 +10,7 @@ struct Rectangle {
 
 impl Rectangle {
     // Only change the test functions themselves
-    pub fn new(width: i32, height: i32) -> Self {
+    pub fn new(width: i32, height: i32) -> Rectangle {
         if width <= 0 || height <= 0 {
             panic!("Rectangle width and height cannot be negative!")
         }
@@ -27,19 +26,19 @@ mod tests {
     fn correct_width_and_height() {
         // This test should check if the rectangle is the size that we pass into its constructor
         let rect = Rectangle::new(10, 20);
-        assert_eq!(???, 10); // check width
-        assert_eq!(???, 20); // check height
+        assert_eq!(rect.width, 10); // check width
+        assert_eq!(rect.height, 20); // check height
     }
 
     #[test]
     fn negative_width() {
         // This test should check if program panics when we try to create rectangle with negative width
-        let _rect = Rectangle::new(-10, 10);
+        let _rect = Rectangle::new(10, 10);
     }
 
     #[test]
     fn negative_height() {
         // This test should check if program panics when we try to create rectangle with negative height
-        let _rect = Rectangle::new(10, -10);
+        let _rect = Rectangle::new(10, 10);
     }
 }
